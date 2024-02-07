ec5453ea16ea75115c9ff20c8cc41fbaeaba993c
akashhosalli <akashdtdm@rediffmail.com>
2024-02-07 02:17:01 +0000

    datatype file got added

---
+++ datatypes.java
@@ -0,0 +1,11 @@
+Class Demo{
+		public static void main(string[]agrs)}
+		int a=30;
+		int b=40;
+		System.out.println(a+b);
+		String x="hello";
+		String y="system";
+		System.out.println(a+b+x+y);
+		}
+}
+
